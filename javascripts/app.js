cdc5467ae3bb0e1a6387be42a49f9ae29fdebed1
Chris Matthias <chris.matthias@gmail.com>
2013-10-04 13:49:09 -0400

    changing field

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,4 +3,5 @@
 ;{% include js/libs/foundation.min.ours.js %}
 {% include js/libs/picturefill.js %}
 {% include js/libs/jquery.backstretch.min.js %}
+{% include js/libs/soundcloud.sdk.js %}
 {% include js/app.js %}
