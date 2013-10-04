22f26c9055b1bc5224f6d64956af2cf3e54f9a13
Chris Matthias <chris.matthias@gmail.com>
2013-10-04 14:16:46 -0400

    adding soundcloud

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,5 +3,5 @@
 ;{% include js/libs/foundation.min.ours.js %}
 {% include js/libs/picturefill.js %}
 {% include js/libs/jquery.backstretch.min.js %}
-{% include js/libs/soundcloud.sdk.js %}
+{% comment %}{% include js/libs/soundcloud.sdk.js %}{% endcomment %}
 {% include js/app.js %}
