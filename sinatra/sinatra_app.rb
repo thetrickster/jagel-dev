5b725db142c4eb7aaf8f7b9e52fd43760626c216
Chris Matthias <chris.matthias@gmail.com>
2013-10-24 21:58:36 -0400

    adding sendgrid for the contact form

--- sinatra/sinatra_app.rb
+++ sinatra/sinatra_app.rb
@@ -2,6 +2,22 @@ set :public_dir, Proc.new { File.join(root, "_site") }
 set :views, Proc.new { File.join(File.dirname(__FILE__), "views") }
 
 post '/contact', :provides => :json do
+  require 'pony'
+  Pony.options = {
+    :via => :smtp,
+    :via_options => {
+      :address => 'smtp.sendgrid.net',
+      :port => '587',
+      :domain => 'heroku.com',
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
+  }
+  Pony.mail(
+    from: "info@jasonagel.com"
+  )
   #
   #
   # Code to handle the data sumbitted by the contact form
